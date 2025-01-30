90339b80d9d07567d50753bd30f3c74fd5d7e5bc
Muhammad Hamza <hamzza07x@gmail.com>
2025-01-30 21:40:03 +0500

    Update color.h

--- color.h
+++ color.h
@@ -1,9 +1,10 @@
 #ifndef COLOR_H
 #define COLOR_H
-#define RED "\033[31m"
-#define GREEN "\033[32m"
-#define BLUE "\033[34m"
-#define PINK "\033[35m"
-#define YELLOW "\033[33m"
-#define RESET "\033[0m"
+//ANSI codes for text color formating in terminal
+#define RED "\033[31m"//RED color for error messeges or alerts
+#define GREEN "\033[32m"//GREEN color for success messeges or prompts
+#define BLUE "\033[34m"//BLUE color for informational messeges
+#define PINK "\033[35m"//PINK color for additional highlights
+#define YELLOW "\033[33m"//YELLOW color for key details or warnings
+#define RESET "\033[0m"//RESET color to default after applying any color
 #endif
