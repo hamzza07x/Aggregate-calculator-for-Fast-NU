ceb7cf69af42ae048b3a1f0134d4f43e32e571e4
Muhammad Hamza <hamzza07x@gmail.com>
2025-01-30 21:35:06 +0500

    Update Aggregate.h

--- Aggregate.h
+++ Aggregate.h
@@ -3,29 +3,42 @@
 #include"Section.h"
 #include<string>
 using namespace std;
+// class aggregate calculator
+//the class calculates aggregate based on test and acadamic scores
 class AggregateCalculator {
 private:
+	//sections of entry test
 	AdvanceMath advanceMath;
 	BasicMath basicMath;
 	Intellegence intellegence;
 	English english;
-	string Name;
-	string Arn;
-	double  matricMarks;
-	double intermidiateMarks;
-	double matricPercentage;
-	double intermidiatePercentage;
-	double entryTestScore;
-	double totalAggregate;
+
+	string Name;//students name
+	string Arn;//admission roll number
+	//acadamic score
+	double  matricMarks;//marks in matric
+	double intermidiateMarks;//marks in intermidiate
+	double matricPercentage;//percentage in matric
+	double intermidiatePercentage;//percentage in intermediate
+	//entry test and aggregate score
+	double entryTestScore;//total scores from entry test sections
+	double totalAggregate;//final aggregate scores
 public:
-	AggregateCalculator();
-	void inputPersonalInfo();
+	AggregateCalculator();//constructor to initialize member variables
+	void inputPersonalInfo();//takes information 
+	//calculate percentage scores in matric and intermediate
 	void calculatePercentages();
+	//take input for all entry test sections
 	void inputAllSections();
+	//calculate the final entry test score
 	double calculateEntryTestScore();
+	//calculate the final aggregate based on acadamic and test weightage
 	void calculateTotalAggregate();
+	//display all scores of individual sections
 	void displayAllScores();
+	//display full summary
 	void displayAll();
+	//destructor of class
 	~AggregateCalculator();
 };
 #endif
